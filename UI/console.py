c67f96f12d01e50399980f1353c5402b60cccb54
Zaharie Stefan <stefan@zaha.tech>
2021-10-25 13:42:19 +0300

    Adaugare UI

---
+++ UI/console.py
@@ -0,0 +1,80 @@
+from Logic.CRUD import *
+from Domain.cheltuiala import creeaza_cheltuiala
+
+
+def ui_adaugare_cheltuiala(lista):
+    id = input("Introduceti id: ")
+    while get_by_id(id, lista) is not None:
+        print("Id deja existent!")
+        id = input("Introduceti id: ")
+
+    nr_apartament = int(input("Introduceti nr apartamentului: "))
+    suma = float(input("Introduceti suma: "))
+    data = input("Intrdouceti data: ")
+    tip = input("Introduceti tipul cheltuielii: ")
+
+    lista = adaugare_cheltuiala(
+        creeaza_cheltuiala(nr_apartament, suma, data, tip, id), lista
+    )
+    return lista
+
+
+def ui_stergere_cheltuiala(lista):
+    id = input("Introduceti id-ul cheltuielii: ")
+    lista = stergere_cheltuiala(id, lista)
+    return lista
+
+
+def ui_modificare_cheltuiala(lista):
+    id = input("Introduceti id-ul cheltuielii: ")
+    if get_by_id(id, lista) is None:
+        print("Id inexistent!")
+        return lista
+
+    nr_apartament = int(input("Introduceti nr apartament: "))
+    suma = float(input("Intoduceti suma: "))
+    data = input("Introduceti data: ")
+    tip = input("Introduceti tipul cheltuielii: ")
+
+    lista = modificare_cheltuiala(id, nr_apartament, suma, data, tip, lista)
+    return lista
+
+
+def print_usage():
+    usage = """
+a. Afisare lista cheltuieli
+m. Afisare meniu
+x. Iesire
+
+1. Adaugare cheltuiala
+2. Stergere cheltuiala
+3. Modificare cheltuiala
+    """
+    print(usage)
+
+
+def run_menu(lista):
+    print_usage()
+
+    while True:
+        optiune = input("Introduceti optiunea: ")
+
+        if optiune == "x":
+            break
+        elif optiune == "a":
+            for cheltuiala in lista:
+                print(cheltuiala)
+        elif optiune == "m":
+            print_usage()
+
+        elif optiune == "1":
+            lista = ui_adaugare_cheltuiala(lista)
+        elif optiune == "2":
+            lista = ui_stergere_cheltuiala(lista)
+        elif optiune == "3":
+            lista = ui_modificare_cheltuiala(lista)
+
+        else:
+            print("Optiune inexistenta!")
+
+    return lista
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from Tests.run_all_tests import run_all_tests
+from UI.console import run_menu
 
 
 def main():
     run_all_tests()
+
+    lista = []
+    run_menu(lista)
 
 
 if __name__ == "__main__":
