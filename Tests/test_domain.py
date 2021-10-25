4ccd6032b3b4b0aa25b4369cb47600198a8dbb22
Zaharie Stefan <stefan@zaha.tech>
2021-10-25 11:58:42 +0300

    Missed test

--- Tests/test_domain.py
+++ Tests/test_domain.py
@@ -2,9 +2,10 @@ from Domain.cheltuiala import *
 
 
 def test_cheltuiala():
-    cheltuiala = creeaza_cheltuiala(40, 230, "25.04.2021", "canal")
+    cheltuiala = creeaza_cheltuiala(40, 230, "25.04.2021", "canal", 9)
 
     assert get_nr_aparament(cheltuiala) == 40
     assert get_suma(cheltuiala) == 230
     assert get_data(cheltuiala) == "25.04.2021"
     assert get_tip(cheltuiala) == "canal"
+    assert get_id(cheltuiala) == 9
